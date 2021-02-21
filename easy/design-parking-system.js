147c75ede855e8c8f7d7ff57ab4c493e9291812c
cruzgerman216 <cruzgerman216@yahoo.com>
2021-02-21 13:37:56 -0600

    design-parking-systems

--- easy/design-parking-system.js
+++ easy/design-parking-system.js
@@ -1,28 +1,24 @@
-
-var ParkingSystem = function(big = 1, medium = 1, small = 0) {
+var ParkingSystem = function(big = 0, medium = 0, small = 3) {
     this.big = big;
     this.medium = medium;
-    this.small = smal;;
+    this.small = small;
 };
 
 
 ParkingSystem.prototype.addCar = function(carType) {
-    if(carType == '1' && this.big != 0){
+    if(carType == 1 && this.big != 0){
         this.big -= 1;
         return true;
-    }else{
-        false;
-    }
-    if(carType == '2' && this.medium != 0){
+    }else if(carType == 2 && this.medium != 0){
         this.medium -= 1;
         return true;
-    }else{
-        false;
-    }
-    if(carType == '3' && this.small != 0){
+    }else if(carType == 3 && this.small != 0){
         this.small -= 1;
         return true;
     }else{
-        false;
+        return false;
     }
 };
+
+p = new ParkingSystem();
+console.log(p.addCar(3));
