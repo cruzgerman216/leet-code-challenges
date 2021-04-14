7afa7fab212046af23bddaf33657778984ac4071
cruzgerman216 <cruzgerman216@yahoo.com>
2021-04-14 18:35:56 -0500

    count good triplets

--- easy/count-good-triplets.js
+++ easy/count-good-triplets.js
@@ -1,11 +1,16 @@
 var countGoodTriplets = function(arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3) {
+    let count = 0;
     for(let i = 0; i < arr.length; i++){
         for(let j = i+1; j < arr.length; j++){
             for(let k = j+1; k < arr.length; k++){
-                if(arr[i] - arr[j] <= a && arr[j] - arr[k] <= b && arr[i] - arr[k] <= c){
-                    console.log("test")
+                if(Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c){
+                    count++;
                 }
             }
         }
     }
+
+    return count;
 };
+
+countGoodTriplets();
