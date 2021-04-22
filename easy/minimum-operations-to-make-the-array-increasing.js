6abec095578031544595ea457565d36750aa8e63
cruzgerman216 <cruzgerman216@yahoo.com>
2021-04-22 17:53:04 -0500

    minimum operations to make the array increasing

--- easy/minimum-operations-to-make-the-array-increasing.js
+++ easy/minimum-operations-to-make-the-array-increasing.js
@@ -1,7 +1,18 @@
-var minOperations = function(nums) {
-    
+var minOperations = function(nums = [1,1,1]) {
+    let operations = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i+1] && nums[i] >= nums[i+1]){
+            let difference = 0;
+            difference = 1 + nums[i] - nums[i+1];
+            nums[i+1] = difference + nums[i+1];
+            operations += difference;
+        }
+    }
+    return operations;
 };
 
+
+console.log(minOperations())
 // 1 5 6 7 8
 
 // 4 3 14
