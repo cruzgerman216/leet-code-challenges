43b0fc9b0c9bc9014c9572b09715f1d2a74d008d
cruzgerman216 <cruzgerman216@yahoo.com>
2021-04-22 19:46:06 -0500

    minimum time visiting all points

--- easy/minimum-time-visiting-all-points.js
+++ easy/minimum-time-visiting-all-points.js
@@ -1,21 +1,28 @@
 var minTimeToVisitAllPoints = function(points = [[1,1],[3,4],[-1,0]]) {
-    let xmin = 0, xmax = 0, ymin = 0, ymax = 0;
+    let operations = 0;
 
     for(let i = 0; i < points.length; i++){
-        if(points[i][0] < xmin){
-            xmin = points[i][0]
-        }
-        if(points[i][0] > xmax){
-            xmax = points[i][0]
-        }
-        if(points[i][1] < ymin){
-            ymin = points[i][1]
-        }
-        if(points[i][1] < ymax){
-            ymax = points[i][]
+        let x,y;
+        if(points[i+1]){
+            if(Math.sign(points[i][0]) == Math.sign(points[i+1][0])){
+                x = Math.abs(points[i][0] - points[i+1][0])
+            }else{
+                x = Math.abs(points[i][0]) + Math.abs(points[i+1][0]);
+            }
+            if(Math.sign(points[i][1]) == Math.sign(points[i+1][1])){
+                y = Math.abs(points[i][1] - points[i+1][1])
+            }else{
+                y = Math.abs(points[i][1]) + Math.abs(points[i+1][1]);
+            }
+        
+            let max = Math.max(x,y);
+            operations += max
+        }else{
+            break;
         }
     }
-    console.log(xmin,xmax,ymin,ymax);
+    return operations
+
 };
 
-minTimeToVisitAllPoints()
+console.log(minTimeToVisitAllPoints())
