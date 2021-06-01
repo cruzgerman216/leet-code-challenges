62e660c3aa8343f2334d0737e7775165139858bd
cruzgerman216 <cruzgerman216@yahoo.com>
2021-05-31 22:59:47 -0500

    GC: reverse integer

---
+++ easy/reverse-integer.js
@@ -0,0 +1,12 @@
+var reverse = function (x = 1534236469) {
+  const num = parseInt(x.toString().split("").reverse().join(""));
+  if (num > 0x7fffffff) {
+    return 0;
+  }
+  if (Math.sign(x) == -1) {
+    return num * -1;
+  }
+  return num;
+};
+
+console.log(reverse());
