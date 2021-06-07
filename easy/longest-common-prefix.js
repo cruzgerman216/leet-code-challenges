5bd80272a04325539eef629b65241efa72d63e20
cruzgerman216 <cruzgerman216@yahoo.com>
2021-06-07 17:56:55 -0500

    GC: longested common prefix second solution

--- easy/longest-common-prefix.js
+++ easy/longest-common-prefix.js
@@ -28,3 +28,20 @@ var longestCommonPrefix = function (strs = ["a"]) {
 };
 
 longestCommonPrefix();
+
+
+// Solution 2
+
+function longestCommonPrefix(strs) {
+    if (!strs.length) return '';
+    
+    for (let i = 0; i < strs[0].length; i++) {
+      for (let str of strs) {
+        if (str[i] !== strs[0][i]) {
+          return str.slice(0, i);
+        }
+      }
+    }
+    
+    return strs[0];
+  }
