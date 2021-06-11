cc54bfe36f62d17e18c4489e5dd1eb7fb18b716c
cruzgerman216 <cruzgerman216@yahoo.com>
2021-06-10 22:52:43 -0500

    GC: add strings time complexity O(n) space complexity O(1)

--- easy/add-strings.js
+++ easy/add-strings.js
@@ -1,18 +1,15 @@
-var addStrings = function (num1 = "11", num2 = "123") {
-  let i = num1.length - 1;
-  let j = num2.length - 1;
-  let carry = 0;
-  let sum = 0;
-  let arr = [];
-  while (i >= 0 || j >= 0 || carry == 1) {
-    sum = (+num1[i] || 0) + (+num2[j] || 0) + carry;
-    sum >= 10 ? (carry = 1) : (carry = 0);
-    arr.unshift(sum % 10);
-    i--;
-    j--;
-  }
+var arrangeCoins = function (n = 8) {
+  let staircases = 0;
+  let coins = n;
 
-  return arr.join("");
+  for (let i = 0; i < n; i++) {
+    staircases++;
+    coins -= staircases;
+    if(staircases >= coins){
+        break;
+    }
+  }
+  return staircases;
 };
 
-addStrings();
+arrangeCoins();
