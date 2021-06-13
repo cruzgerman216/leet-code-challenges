c95217f40e764ccea87d15f3cab96f4207dcd39a
cruzgerman216 <cruzgerman216@yahoo.com>
2021-06-13 18:24:18 -0500

    GC: assign cookies time complexity O(n) space complexity O(1)

---
+++ easy/assign-cookies.js
@@ -0,0 +1,13 @@
+var findContentChildren = function (g = [10,9,8,7], s = [5,6,7,8]) {
+  let i = 0;
+  let childrenSatisfied = 0;
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  for (let num of s) {
+    if (num >= g[i]) {
+      childrenSatisfied++;
+      i++;
+    }
+  }
+  return childrenSatisfied;
+};
